46209d4e50d73d4bb1c55469067499c628d2d270
Michael Luthor <michaelluthor@163.com>
2018-04-20 17:44:25 +0800

    add sqlloader and queryresponse

---
+++ Framework/Service/XDatabase/Core/Database/QueryResponse.php
@@ -0,0 +1,3 @@
+<?php
+namespace X\Service\XDatabase\Core\Database;
+class QueryResponse {}
---
+++ Framework/Service/XDatabase/Core/SQL/SqlLoader.php
@@ -0,0 +1,11 @@
+<?php 
+namespace X\Service\XDatabase\Core\SQL;
+class SqlLoader {
+    /** @return self */
+    public static function load($path) {}
+    public function setParams($params) {}
+    public function query($database) {}
+    /** @return self */
+    public function setDatabase() {}
+    public function exec($database) {}
+}

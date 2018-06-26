78e5dee4bdbe0bb02250946fcc26aa1f288863f4
Michael Luthor <michaelluthor@163.com>
2018-06-26 17:05:09 +0800

    add query condition

---
+++ Framework/Service/Database/Query/Condition.php
@@ -0,0 +1,494 @@
+<?php
+namespace X\Service\Database\Query;
+use X\Service\Database\Database;
+use X\Service\Database\DatabaseException;
+/**
+ * @author Michael Luthor <michaelluthor@163.com>
+ */
+class Condition {
+    /** condition type for condition  */
+    const TYPE_CONDITION = 1;
+    /** condition type for connector  */
+    const TYPE_CONNECTOR = 2;
+    /** condition type for custom condition  */
+    const TYPE_CUSTOM_CONDITION = 3;
+    
+    /** @var Database */
+    private $database = null;
+    /** @var array */
+    private $params = array();
+    /** @var string */
+    private $cache = null;
+    /** @var mixed */
+    private $conditions = array(
+        # array( 'type' => 'condition', content => '1=1' )
+        # array( 'type' => 'connector', content=>and )
+    );
+    
+    /** @return self */
+    public static function build( ) {
+        return new static();
+    }
+    
+    /** @return self */
+    public function setDatabase( Database $db ) {
+        $this->cache = null;
+        $this->database = $db;
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return self
+     */
+    public function is( $name, $value ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => '=',
+                'value' => $value,
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return self
+     */
+    public function isNot( $name, $value ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => '<>',
+                'value' => $value,
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return self
+     */
+    public function equals( $name, $value ) {
+        $this->cache = null;
+        return $this->is($name, $value);
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return self
+     */
+    public function notEquals( $name, $value ) {
+        $this->cache = null;
+        return $this->isNot($name, $value);
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return self
+     */
+    public function lessThan( $name, $value ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => '<',
+                'value' => $value,
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return self
+     */
+    public function lessOrEqual( $name, $value ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => '<=',
+                'value' => $value,
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return self
+     */
+    public function greaterThan( $name, $value ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => '>',
+                'value' => $value,
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return self
+     */
+    public function greaterOrEqual( $name, $value ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => '>=',
+                'value' => $value,
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return self
+     */
+    public function contains( $name, $value ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => 'LIKE',
+                'value' => '%'.$value.'%',
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return self
+     */
+    public function notContains( $name, $value ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => 'NOT LIKE',
+                'value' => '%'.$value.'%',
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return self
+     */
+    public function beginWith( $name, $value ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => 'LIKE',
+                'value' => $value.'%',
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return self
+     */
+    public function endWith( $name, $value ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => 'LIKE',
+                'value' => '%'.$value,
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @return self
+     */
+    public function isNull( $name ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => 'IS NULL',
+                'value' => null,
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @return self
+     */
+    public function isNotNull( $name ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => 'IS NOT NULL',
+                'value' => null,
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $min
+     * @param mixed $max
+     * @return self
+     */
+    public function between( $name, $min, $max ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => 'BETWEEN',
+                'value' => array('min'=>$min, 'max'=>$max),
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $min
+     * @param mixed $max
+     * @return self
+     */
+    public function notBetween( $name, $min, $max ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => 'NOT BETWEEN',
+                'value' => array('min'=>$min, 'max'=>$max),
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $list
+     * @return self
+     */
+    public function in( $name, $list ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => 'IN',
+                'value' => $list,
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string $name
+     * @param mixed $list
+     * @return self
+     */
+    public function notIn( $name, $list ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONDITION,
+            'content' => array(
+                'expr' => $name,
+                'operator' => 'NOT IN',
+                'value' => $list,
+            ),
+        );
+        return $this;
+    }
+    
+    /**
+     * @param string|array|self $condition
+     * @return self
+     */
+    public function add($condition) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type'=>self::TYPE_CUSTOM_CONDITION,
+            'content'=> $condition,
+        );
+        return $this;
+    }
+    
+    /** @return self */
+    public function and( ) {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONNECTOR,
+            'content' => 'AND',
+        );
+        return $this;
+    }
+    
+    /** @return self */
+    public function or() {
+        $this->cache = null;
+        $this->conditions[] = array(
+            'type' => self::TYPE_CONNECTOR,
+            'content' => 'OR',
+        );
+        return $this;
+    }
+    
+    /**
+     * @param array $params
+     * @return \X\Service\Database\Query\Condition
+     */
+    public function setPreviousParams( &$params ) {
+        $this->params = &$params;
+        return $this;
+    }
+    
+    /** @return array */
+    public function getBindParams() {
+        if ( null === $this->cache ) {
+            $this->toString();
+        }
+        return $this->params;
+    }
+    
+    /** @return string  */
+    public function toString() {
+        if ( null !== $this->cache ) {
+            return $this->cache;
+        }
+        
+        $conditions = array();
+        $lastConditionElemType = null;
+        foreach ( $this->conditions as $condition ) {
+            $conditionStr = null;
+            
+            if ( self::TYPE_CONNECTOR === $condition['type'] ) {
+                $conditionStr = $condition['content'];
+            } else if ( self::TYPE_CONDITION === $condition['type'] ){
+                $conditionStr = $this->convertNormalConditionToString($condition['content']);
+            } else if ( self::TYPE_CUSTOM_CONDITION === $condition['type'] ) {
+                $conditionStr = $this->convertCustomConditionToString($condition['content']);
+            }
+            
+            if ( null !== $lastConditionElemType 
+            && self::TYPE_CONNECTOR !== $lastConditionElemType 
+            && self::TYPE_CONNECTOR !== $condition['type'] ) {
+                $conditions[] = 'AND';
+            }
+            $conditions[] = $conditionStr;
+            $lastConditionElemType = $condition['type'];
+        }
+        
+        if ( self::TYPE_CONNECTOR === $lastConditionElemType ) {
+            array_pop($conditions);
+        }
+        
+        $this->cache = '( '.implode(' ', $conditions).' )';
+        return $this->cache;
+    }
+    
+    /**
+     * @param mixed $condition
+     * @return string
+     */
+    private function convertCustomConditionToString( $condition ) {
+        if ( $condition instanceof Condition ) {
+            $condition->setPreviousParams($this->params);
+            $condition->setDatabase($this->database);
+            return $condition->toString();
+        } else if ( is_string($condition) ) {
+            return $condition;
+        } else if ( is_array($condition) ) {
+            foreach ( $condition as $key => $value ) {
+                $content = array('expr' => $key,'operator' => '=','value' => $value);
+                return $this->convertNormalConditionToString($content);
+            }
+        } else {
+            throw new DatabaseException('custom condition format error');
+        }
+    }
+    
+    /**
+     * @param array $condition
+     * @return string
+     */
+    private function convertNormalConditionToString( array $condition ) {
+        $expr = $this->database->quoteColumnName($condition['expr']);
+        
+        switch ( $condition['operator'] ) {
+        case 'IS NULL': 
+        case 'IS NOT NULL': 
+            return "{$expr} {$condition['operator']}";
+        case 'NOT BETWEEN' :
+        case 'BETWEEN' :
+            $paramsKeyMin = ':qp'.count($this->params);
+            $this->params[$paramsKeyMin] = $condition['value']['min'];
+            $paramsKeyMax = ':qp'.count($this->params);
+            $this->params[$paramsKeyMax] = $condition['value']['max'];
+            return "{$expr} {$condition['operator']} {$paramsKeyMin} AND {$paramsKeyMax}";
+        case 'IN' :
+        case 'NOT IN' :
+            $markList = array();
+            foreach ( $condition['value'] as $value ) {
+                $paramsKey = ':qp'.count($this->params);
+                $this->params[$paramsKey] = $value;
+                $markList[] = $paramsKey;
+            }
+            $markList = implode(', ', $markList);
+            return "{$expr} {$condition['operator']} ( $markList )";
+        default :
+            $paramsKey = ':qp'.count($this->params);
+            $this->params[$paramsKey] = $condition['value'];
+            return "{$expr} {$condition['operator']} {$paramsKey}";
+        }
+    }
+    
+    /** @return string  */
+    public function __toString() {
+        return $this->toString();
+    }
+}
---
+++ Framework/Service/Database/Test/Service/Query/ConditionTest.php
@@ -0,0 +1,201 @@
+<?php
+namespace X\Service\Database\Test\Service\Query;
+use X\Core\X;
+use PHPUnit\Framework\TestCase;
+use X\Service\Database\Database;
+use X\Service\Database\Query\Condition;
+class ConditionTest extends TestCase {
+    private $db = null;
+    
+    /**
+     * {@inheritDoc}
+     * @see \PHPUnit\Framework\TestCase::setUp()
+     */
+    protected function setUp() {
+        $config = X::system()->getConfiguration()->get('params')->get('MysqlDriverConfig');
+        $db = new Database($config);
+        $this->db = $db;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \PHPUnit\Framework\TestCase::tearDown()
+     */
+    protected function tearDown() {
+        $this->db = null;
+    }
+    
+    /** */
+    public function test_group() {
+        $condition = Condition::build()
+            ->add(Condition::build()->is('name', 'michael')->is('sex', 'm'))
+            ->or()
+            ->add(Condition::build()->is('name','lois')->is('sex', 'f'));
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( ( `name` = :qp0 AND `sex` = :qp1 ) OR ( `name` = :qp2 AND `sex` = :qp3 ) )', $condition->toString());
+        $this->assertEquals(array (
+            ':qp0' => 'michael',
+            ':qp1' => 'm',
+            ':qp2' => 'lois',
+            ':qp3' => 'f',
+        ), $condition->getBindParams());
+    }
+    
+    /**  */
+    public function test_connectors() {
+        # mutil conditions
+        $condition = Condition::build()
+            ->is('name', 'michael')
+            ->lessOrEqual('age', 10)
+            ->is('sex', 'm');
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `name` = :qp0 AND `age` <= :qp1 AND `sex` = :qp2 )', $condition->toString());
+        $this->assertEquals(array(
+            ':qp0' => 'michael',
+            ':qp1' => 10,
+            ':qp2' => 'm'
+        ), $condition->getBindParams());
+        
+        
+        # simply and or
+        $condition = Condition::build()
+            ->is('name', 'michael')
+            ->and()
+            ->is('sex', 'm')
+            ->or()
+            ->is('age', 10);
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `name` = :qp0 AND `sex` = :qp1 OR `age` = :qp2 )', $condition->toString());
+        $this->assertEquals(array(
+            ':qp0' => 'michael',
+            ':qp1' => 'm',
+            ':qp2' => 10,
+        ), $condition->getBindParams());
+    }
+    
+    /**  */
+    public function test_custom_condition() {
+        # string
+        $condition = Condition::build()->add('age=1');
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( age=1 )', $condition->toString());
+        
+        # array 
+        $condition = Condition::build()->add(array(
+            'name' => 'michael',
+        ));
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `name` = :qp0 )', $condition->toString());
+        $this->assertEquals(array(
+            ':qp0' => 'michael',
+        ), $condition->getBindParams());
+        
+        # Condition object
+        $conditionTmp = Condition::build()->is('name', 'michael');
+        $condition = Condition::build()->add($conditionTmp);
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( ( `name` = :qp0 ) )', $condition->toString());
+        $this->assertEquals(array(
+            ':qp0' => 'michael',
+        ), $condition->getBindParams());
+    }
+    
+    /** */
+    public function test_operators() {
+        # =
+        $condition = Condition::build()->is('name', 'michael');
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `name` = :qp0 )', $condition->toString());
+        $this->assertEquals(array(':qp0'=>'michael'), $condition->getBindParams());
+        
+        # <>
+        $condition = Condition::build()->isNot('name', 'michael');
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `name` <> :qp0 )', $condition->toString());
+        $this->assertEquals(array(':qp0'=>'michael'), $condition->getBindParams());
+        
+        # <
+        $condition = Condition::build()->lessThan('age', 10);
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `age` < :qp0 )', $condition->toString());
+        $this->assertEquals(array(':qp0'=>10), $condition->getBindParams());
+        
+        # <=
+        $condition = Condition::build()->lessOrEqual('age', 10);
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `age` <= :qp0 )', $condition->toString());
+        $this->assertEquals(array(':qp0'=>10), $condition->getBindParams());
+        
+        # >
+        $condition = Condition::build()->greaterThan('age', 10);
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `age` > :qp0 )', $condition->toString());
+        $this->assertEquals(array(':qp0'=>10), $condition->getBindParams());
+        
+        # >=
+        $condition = Condition::build()->greaterOrEqual('age', 10);
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `age` >= :qp0 )', $condition->toString());
+        $this->assertEquals(array(':qp0'=>10), $condition->getBindParams());
+        
+        # contains
+        $condition = Condition::build()->contains('name', 'michael');
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `name` LIKE :qp0 )', $condition->toString());
+        $this->assertEquals(array(':qp0'=>'%michael%'), $condition->getBindParams());
+        
+        # not contains
+        $condition = Condition::build()->notContains('name', 'michael');
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `name` NOT LIKE :qp0 )', $condition->toString());
+        $this->assertEquals(array(':qp0'=>'%michael%'), $condition->getBindParams());
+        
+        # beginWith
+        $condition = Condition::build()->beginWith('name', 'michael');
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `name` LIKE :qp0 )', $condition->toString());
+        $this->assertEquals(array(':qp0'=>'michael%'), $condition->getBindParams());
+        
+        # endWith
+        $condition = Condition::build()->endWith('name', 'michael');
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `name` LIKE :qp0 )', $condition->toString());
+        $this->assertEquals(array(':qp0'=>'%michael'), $condition->getBindParams());
+        
+        # isNUll
+        $condition = Condition::build()->isNull('name');
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `name` IS NULL )', $condition->toString());
+        $this->assertEquals(array(), $condition->getBindParams());
+        
+        # isNotNull
+        $condition = Condition::build()->isNotNull('name');
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `name` IS NOT NULL )', $condition->toString());
+        $this->assertEquals(array(), $condition->getBindParams());
+        
+        # between
+        $condition = Condition::build()->between('age', 10, 20);
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `age` BETWEEN :qp0 AND :qp1 )', $condition->toString());
+        $this->assertEquals(array(':qp0'=>10,':qp1'=>20), $condition->getBindParams());
+        
+        # not between
+        $condition = Condition::build()->notBetween('age', 10, 20);
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `age` NOT BETWEEN :qp0 AND :qp1 )', $condition->toString());
+        $this->assertEquals(array(':qp0'=>10,':qp1'=>20), $condition->getBindParams());
+        
+        # in
+        $condition = Condition::build()->in('age', array(10, 20, 30));
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `age` IN ( :qp0, :qp1, :qp2 ) )', $condition->toString());
+        $this->assertEquals(array(':qp0'=>10,':qp1'=>20,':qp2'=>30), $condition->getBindParams());
+        
+        # notin
+        $condition = Condition::build()->notIn('age', array(10, 20, 30));
+        $condition->setDatabase($this->db);
+        $this->assertEquals('( `age` NOT IN ( :qp0, :qp1, :qp2 ) )', $condition->toString());
+        $this->assertEquals(array(':qp0'=>10,':qp1'=>20,':qp2'=>30), $condition->getBindParams());
+    }
+}

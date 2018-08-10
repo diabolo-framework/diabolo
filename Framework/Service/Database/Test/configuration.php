4c9902fcc81919dc7c23b38ecce2237ecfe51349
Michael Luthor <michaelluthor@163.com>
2018-08-10 09:41:32 +0800

    update test configuration.

--- Framework/Service/Database/Test/configuration.php
+++ Framework/Service/Database/Test/configuration.php
@@ -21,42 +21,42 @@ return array(
         'delay' => true,
         'params' => array(
             'databases' => array(
-//                 'mysqlTestDB' => array(
-//                     'driver' => Mysql::class,
-//                     'host' => '127.0.0.1',
-//                     'username' => 'root',
-//                     'password' => '',
-//                     'charset' => 'UTF8',
-//                     'dbname' => 'diabolo.test',
-//                 ),
-//                 'sqliteTestDB' => array(
-//                     'driver' => Sqlite::class,
-//                     'path' => __DIR__.'/Resource/Data/diabolo.db',
-//                 ),
-//                 'postgresqlTestDB' => array(
-//                     'driver' => Postgresql::class,
-//                     'host' => '106.12.16.95',
-//                     'username' => 'postgres',
-//                     'password' => 'ginhappy',
-//                     'dbname' => 'diabolo.test',
-//                     'port' => 5432
-//                 ),
-//                 'oracleTestDB' => array(
-//                     'driver' => Oracle::class,
-//                     'host' => '192.168.1.102',
-//                     'username' => 'C##DIABOLO',
-//                     'password' => 'C##DIABOLO',
-//                     'serviceName' => 'ORCL',
-//                     'port' => 1521
-//                 ),
-//                 'mssqlTestDB' => array(
-//                     'driver' => Mssql::class,
-//                     'host' => '192.168.1.104',
-//                     'username' => 'diabolo',
-//                     'password' => 'diabolo',
-//                     'dbname' => 'diabolo',
-//                     'port' => 1433
-//                 ),
+                'mysqlTestDB' => array(
+                    'driver' => Mysql::class,
+                    'host' => '127.0.0.1',
+                    'username' => 'root',
+                    'password' => '',
+                    'charset' => 'UTF8',
+                    'dbname' => 'diabolo.test',
+                ),
+                'sqliteTestDB' => array(
+                    'driver' => Sqlite::class,
+                    'path' => __DIR__.'/Resource/Data/diabolo.db',
+                ),
+                'postgresqlTestDB' => array(
+                    'driver' => Postgresql::class,
+                    'host' => '106.12.16.95',
+                    'username' => 'postgres',
+                    'password' => 'ginhappy',
+                    'dbname' => 'diabolo.test',
+                    'port' => 5432
+                ),
+                'oracleTestDB' => array(
+                    'driver' => Oracle::class,
+                    'host' => '192.168.1.102',
+                    'username' => 'C##DIABOLO',
+                    'password' => 'C##DIABOLO',
+                    'serviceName' => 'ORCL',
+                    'port' => 1521
+                ),
+                'mssqlTestDB' => array(
+                    'driver' => Mssql::class,
+                    'host' => '192.168.1.104',
+                    'username' => 'diabolo',
+                    'password' => 'diabolo',
+                    'dbname' => 'diabolo',
+                    'port' => 1433
+                ),
                 'firebirdTestDB' => array(
                     'driver' => Firebird::class,
                     'host' => '192.168.1.105',
